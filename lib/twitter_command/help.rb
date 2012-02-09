cef6db317a2dc2fa5998478a035134c28c309975
neal paulownia <hisary+dev@gmail.com>
2012-02-10 00:16:02 +0900

    rename subcommand users-lookup to screen-name

--- lib/twitter_command/help.rb
+++ lib/twitter_command/help.rb
@@ -10,7 +10,7 @@ commands:
   test: check twitter api alive.
   followers: print user_ids for users following you.
   friends: print user_ids that you follow.
-  users-lookup: convert user_id to screen_name.
+  screen-name: convert user_id to screen_name.
   no-retweet: print user_ids that you don't want to see retweets from.
   blocking: print user_ids that you are blocking.
   following: print ids for every user you follow who don't follow you back.
